83ed9edeb563e3df4ca4623fffe3b8caba6df3b4
amine374 <aminepro924@gmail.com>
2021-03-10 15:08:29 +0100

    second exercice is added

---
+++ EXO2_TP2/EXO2_TP2/App_Code/Etudiant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Etudiant
+/// </summary>
+public class Etudiant
+{
+    public String nom;
+    public String prenom;
+    public int age;
+    public Etudiant(String nom, String prenom, int age)
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+}
