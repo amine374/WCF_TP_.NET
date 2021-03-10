2e70c048c715930283fae3d7bf9b015d26e6c77d
amine374 <aminepro924@gmail.com>
2021-03-10 15:05:59 +0100

    First exercice is added

---
+++ WCFService1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.ServiceClient pro = new ServiceReference1.ServiceClient();
+            String name = textBox1.Text;
+            label2.Text = pro.message(name);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
